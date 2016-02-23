72d79dbf76f3a2848e33c0b2298388525e3eb716
shivasdst <shivably04sdst@gmail.com>
2016-02-23 12:57:23 +0530

    Commit No. 11 - changed path

--- application/config/config.php
+++ application/config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 // define('BASE_URL', 'http://www.ias.ac.in/stage/');
-define('BASE_URL', 'http://localhost/ias/');
+define('BASE_URL', 'http://localhost/iitm/');
 define('PUBLIC_URL', BASE_URL . 'public/');
 define('XML_SRC_URL', BASE_URL . 'md-src/xml/');
 define('VOL_URL', PUBLIC_URL . 'Volumes/');
@@ -11,7 +11,7 @@ define('RESOURCES_URL', PUBLIC_URL . 'Resources/');
 
 // Physical location of resources
 // define('PHY_BASE_URL', '/mnt/datadisk1/ias/');
-define('PHY_BASE_URL', '/var/www/html/ias/');
+define('PHY_BASE_URL', '/var/www/html/iitm/');
 define('PHY_PUBLIC_URL', PHY_BASE_URL . 'public/');
 define('PHY_XML_SRC_URL', PHY_BASE_URL . 'md-src/xml/');
 define('PHY_VOL_URL', PHY_PUBLIC_URL . 'Volumes/');
@@ -21,7 +21,7 @@ define('PHY_FLAT_URL', PHY_BASE_URL . 'application/views/flat/');
 define('PHY_STOCK_IMAGE_URL', PHY_PUBLIC_URL . 'images/stock/');
 define('PHY_RESOURCES_URL', PHY_PUBLIC_URL . 'Resources/');
 
-define('DB_PREFIX', 'ias');
+define('DB_PREFIX', 'iitm');
 define('DB_HOST', 'localhost');
 
 // infra will become iasINFRA inside
