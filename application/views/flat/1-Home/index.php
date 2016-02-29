94caa5c015e93473f725e912e256d60cc9dd7497
arjunkashyap <arjun.kashyap@yahoo.co.in>
2016-02-29 18:03:35 +0530

    Commit No. 29 - Home page - links, physical space and get in touch sections are in place

--- application/views/flat/1-Home/index.php
+++ application/views/flat/1-Home/index.php
@@ -14,6 +14,157 @@
     </div>
 </div>
 <div class="container">
+    <ul class="timeline">
+        <li>
+          <div class="timeline-badge">1956</div>
+          <!-- <div class="timeline-panel">
+            <div class="timeline-heading">
+              <h4 class="timeline-title">Mussum ipsum cacilds</h4>
+              <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 11 hours ago via Twitter</small></p>
+            </div>
+            <div class="timeline-body">
+              <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
+            </div>
+          </div> -->
+          <div class="timeline-panel">
+            <div class="timeline-body">
+              <p>Visit of Prime Minister Jawaharlal Nehru to West Germany and the offer made to him by the Government of the Federal Republic of Germany to assist in the setting up of a higher technological institute in India.</p>
+            </div>
+          </div>
+        </li>
+        <li class="timeline-inverted">
+          <div class="timeline-badge">1958</div>
+          <div class="timeline-panel">
+            <div class="timeline-body">
+              <p>The signing of the first Indo-German agreement in Bonn, West Germany for the establishment of the Indian Institute of Technology at Madras.</p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="timeline-badge">1959</div>
+          <div class="timeline-panel">
+            <div class="timeline-body">
+              <p>Formal inauguration of the Institute by Prof.Humayun Kabir, Union Minister for Scientific Research and Cultural Affairs, Dr.A.Lakshmanaswamy Mudaliar assumes office as the first Chairman of the Board of Governors of the Institute. Prof.B.Sengupto assumes office as the first Director of the Institute.</p>
+            </div>
+          </div>
+        </li>
+        <li class="timeline-inverted">
+          <div class="timeline-badge">1961</div>
+          <div class="timeline-panel">
+            <div class="timeline-body">
+              <p>Declaration of the Institute by the Parliament of India as an institution of National importance.</p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="timeline-badge">1962</div>
+          <div class="timeline-panel">
+            <div class="timeline-body">
+              <p>Visit to the Institute by Dr.Heinrich Lubke, President of the Federal Republic of Germany and the unveiling of a tablet symbolising the Indo-German technical assistance programme.</p>
+            </div>
+          </div>
+        </li>
+        <li class="timeline-inverted">
+          <div class="timeline-badge">1967</div>
+          <div class="timeline-panel">
+            <div class="timeline-body">
+              <p>Dr. A. Ramachandran takes over as Director from Prof. B. Sengupto.</p>
+            </div>
+          </div>
+        </li>
+    </ul>
+</div>
+<div class="container-fluid stats">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>The Collection</h1>
+            <ul class="list-inline">
+                <li class="stat-elem">
+                    <a href="#">
+                        <h2><i class="fa fa-image"></i></h2>
+                        <p>Photographs</p>
+                    </a>
+                </li>
+                <li class="stat-elem">
+                    <a href="#">
+                        <h2><i class="fa fa-book"></i></h2>
+                        <p>Publications</p>
+                    </a>
+                </li>
+                <li class="stat-elem">
+                    <a href="#">
+                        <h2><i class="fa fa-file-text-o"></i></h2>
+                        <p>Brochures</p>
+                    </a>
+                </li>
+                <li class="stat-elem">
+                    <a href="#">
+                        <h2><i class="fa fa-paint-brush"></i></h2>
+                       <p>Paintings</p>
+                    </a>
+                </li>
+                <li class="stat-elem">
+                    <a href="#">
+                        <h2><i class="fa fa-file-video-o"></i></h2>
+                       <p>Videos</p>
+                    </a>
+                </li>
+                <li class="stat-elem">
+                    <a href="#">
+                        <h2><i class="fa fa-trophy"></i></h2>
+                        <p>Artefacts</p>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="container-fluid physical">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Physical Space</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <h2>Inauguration in 2006</h2>
+            <p>In the Centre, about 500 photographs depicting the early history of IITM are on permanent display on 82 Display Panels with a 14 minute in-house documentary, and a recently added Touch Screen Kiosk.</p>
+        </div>
+        <div class="col-md-4">
+            <h2>Renovation underway</h2>
+            <p>In addition, a few interesting photographs were on temporary display. Several other artifacts (IITM Annual Numbers, albums, copies of Campastimes) are also exhibited.</p>
+        </div>
+        <div class="col-md-4">
+            <h2>Contribute</h2>
+            <p>Our alumnus, Kumaran Sathasivam (who has co-authored the IITM Coffee-Table Book with Prof. Ajit Kolar, apart from authoring two lovely books on Campus Flora & Fauna) is now looking after the "Heritage Centre", and the "Golden Reunion" batches of '64 & '65 have agreed to fund its renovation.</p>
+        </div>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="row first-row">
+        <div class="col-md-12 clear-paddings">
+            <div class="fixOverlayDiv">
+                <img class="img-responsive gap-above" src="<?=PUBLIC_URL?>images/stock/slide2.jpg">
+                <div class="OverlayTextMain">
+                    <div class="mainpage">
+                        <h2>Get in touch</h2><br />
+                        <form>
+                            <div class="form-group">
+                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                            </div>
+                            <div class="form-group">
+                                <textarea rows="5" class="form-control" id="exampleInputPassword1" placeholder="Your message here"></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-default naked">Submit</button>
+                        </form>
+                        <p><br /><br /><small>© 2016 Indian Institute of Technology Madras.</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container">
     <div class="row first-row">
         <div class="col-md-9">
             <img class="img-responsive gap-below-med" src="http://placehold.it/850x600">
--- application/views/header.php
+++ application/views/header.php
@@ -31,7 +31,7 @@
     <!-- CSS
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
     <link rel="stylesheet" href="<?=PUBLIC_URL?>css/normalize.css">
-    <link rel="stylesheet" href="<?=PUBLIC_URL?>css/font-awesome-4.1.0/css/font-awesome.min.css" type="text/css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <!-- <link rel="stylesheet" href="css/skeleton.css"> -->
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
