e74f54e821a9f8f22f3bb173666cc7c629812223
arjunkashyap <arjun.kashyap@yahoo.co.in>
2016-03-01 01:38:04 +0530

    Commit No. 34 - unwanted data removed

--- application/config/config.php
+++ application/config/config.php
@@ -1,6 +1,5 @@
 <?php
 
-// define('BASE_URL', 'http://www.ias.ac.in/stage/');
 define('BASE_URL', 'http://localhost/iitm/');
 define('PUBLIC_URL', BASE_URL . 'public/');
 define('XML_SRC_URL', BASE_URL . 'md-src/xml/');
@@ -10,7 +9,6 @@ define('STOCK_IMAGE_URL', PUBLIC_URL . 'images/stock/');
 define('RESOURCES_URL', PUBLIC_URL . 'Resources/');
 
 // Physical location of resources
-// define('PHY_BASE_URL', '/mnt/datadisk1/ias/');
 define('PHY_BASE_URL', '/var/www/html/iitm/');
 define('PHY_PUBLIC_URL', PHY_BASE_URL . 'public/');
 define('PHY_XML_SRC_URL', PHY_BASE_URL . 'md-src/xml/');
@@ -24,80 +22,10 @@ define('PHY_RESOURCES_URL', PHY_PUBLIC_URL . 'Resources/');
 define('DB_PREFIX', 'iitm');
 define('DB_HOST', 'localhost');
 
-// infra will become iitmINFRA inside
+// photo will become iitmPHOTO inside
 define('DB_NAME', 'photo');
 
 define('iitmPHOTO_USER', 'root');
 define('iitmPHOTO_PASSWORD', 'mysql');
-
-
-// IAS Server settings
-
-// define('iasBOMS_USER', 'usrboms');
-// define('iasBOMS_PASSWORD', 'pA5s@b0ms');
-
-// define('iasJBSC_USER', 'usrjbsc');
-// define('iasJBSC_PASSWORD', 'pA5s@jb5c');
-
-// define('iasJCSC_USER', 'usrjcsc');
-// define('iasJCSC_PASSWORD', 'pA5s@jc5c');
-
-// define('iasJESS_USER', 'usrjess');
-// define('iasJESS_PASSWORD', 'pA5s@j3s5');
-
-// define('iasJGEN_USER', 'usrjgen');
-// define('iasJGEN_PASSWORD', 'pA5s@jg3n');
-
-// define('iasJOAA_USER', 'usrjoaa');
-// define('iasJOAA_PASSWORD', 'pA5s@j0aa');
-
-// define('iasPMSC_USER', 'usrpmsc');
-// define('iasPMSC_PASSWORD', 'pA5s@pm5c');
-
-// define('iasPRAM_USER', 'usrpram');
-// define('iasPRAM_PASSWORD', 'pA5s@p6am');
-
-// define('iasRESO_USER', 'usrreso');
-// define('iasRESO_PASSWORD', 'pA5s@r3so');
-
-// define('iasSADH_USER', 'usrsadh');
-// define('iasSADH_PASSWORD', 'pA5s@5adh');
-
-// define('iasINFRA_USER', 'usrinfra');
-// define('iasINFRA_PASSWORD', 'pA5s@inf6a');
-
-
-// Local settings
-
-//~ define('iasBOMS_USER', 'root');
-//~ define('iasBOMS_PASSWORD', 'mysql');
-//~ 
-//~ define('iasJBSC_USER', 'root');
-//~ define('iasJBSC_PASSWORD', 'mysql');
-//~ 
-//~ define('iasJCSC_USER', 'root');
-//~ define('iasJCSC_PASSWORD', 'mysql');
-//~ 
-//~ define('iasJESS_USER', 'root');
-//~ define('iasJESS_PASSWORD', 'mysql');
-//~ 
-//~ define('iasJGEN_USER', 'root');
-//~ define('iasJGEN_PASSWORD', 'mysql');
-//~ 
-//~ define('iasJOAA_USER', 'root');
-//~ define('iasJOAA_PASSWORD', 'mysql');
-//~ 
-//~ define('iasPMSC_USER', 'root');
-//~ define('iasPMSC_PASSWORD', 'mysql');
-//~ 
-//~ define('iasPRAM_USER', 'root');
-//~ define('iasPRAM_PASSWORD', 'mysql');
-//~ 
-//~ define('iasRESO_USER', 'root');
-//~ define('iasRESO_PASSWORD', 'mysql');
-//~ 
-//~ define('iasSADH_USER', 'root');
-//~ define('iasSADH_PASSWORD', 'mysql');
-//~ 
 
 ?>
