4136500834e0e417224e4bf8aeba6dccb03ccd0b
shivasdst <shivably04sdst@gmail.com>
2016-03-23 16:06:23 +0530

    No. 69 - TOC is added in publication page

--- application/views/flat/2-Publications/index.php
+++ application/views/flat/2-Publications/index.php
@@ -28,7 +28,66 @@
             <img class="img-responsive" src="<?=PUBLIC_URL?>images/stock/an-003.jpg">
             <p class="image-desc">
                 <strong>Annual Number 3</strong><br />
-                <a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf" target="_blank">PDF</a>
+                <a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf" target="_blank">PDF</a>  
+                <button id="TOCtoggle" class="toggle-button">TOC</button>
+                <div id="toc">
+                    <p>
+                        <a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=9" target="_blank">Editorial</a><br />
+                        <span class="toc-author">Prof. B. Sengupto</small>
+                    </p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=11" target="_blank">Message</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=13" target="_blank">An Extract</a></p>
+                    <p>
+                        <a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=14" target="_blank">At the Circus</a><br />
+                        <small><i>Dr. Th. Ramachandran</i></small>
+                    </p>
+                    <p>
+                        <a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=18" target="_blank">The Canteen</a><br />
+                        <span class="float-right"><i>G. Viswanathan</i></span>
+                    </p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=19" target="_blank">Damn the Congo</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=22" target="_blank">Dejections</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=25" target="_blank">Noises</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=26" target="_blank">Rock Climbing</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=31" target="_blank">For Official Use Only</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=33" target="_blank">Balmy Days at Brisbane</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=38" target="_blank">The First Five Year Plan</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=38" target="_blank">Summer</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=39" target="_blank">A Wish Granted</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=40" target="_blank">Impressions</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=41" target="_blank">Ashta Graha</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=" target="_blank">Grades are Sour</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=42" target="_blank">The Engineer's Wand - A Fantasy</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=46" target="_blank">The Road to I.I.T.</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=47" target="_blank">A Novel Souvenir</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=50" target="_blank">The Timachine</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=53" target="_blank">German Language</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=54" target="_blank">If I were the Creator</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=" target="_blank">The Buddha</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=" target="_blank">Defence of our Motherland</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=58" target="_blank">The Gestetner Civilisation</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=59" target="_blank">Fly</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=" target="_blank">A Pet Problem</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=60" target="_blank">The Aromatic River</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=61" target="_blank">A Portrait of Nehru</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=63" target="_blank">The Quaint ways in British Parliament</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=71" target="_blank">The Chemistry of an Engineer</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=75" target="_blank">I.I.T. Through Photographs</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=79" target="_blank">I.I.T. Through Cartoons</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=80" target="_blank">Institute Gymkhana</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=88" target="_blank">Chronicle</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=92" target="_blank">A Distinguished visitor to our Campus</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=93" target="_blank">Faculty Association</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=" target="_blank">I.I.T. Co-operative Stores</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=" target="_blank">Students Aid Fund</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=94" target="_blank">Hostels</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=95" target="_blank">I.I.T. Employees Co-operative Housing Society Ltd.</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=96" target="_blank">Tech - Out</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=99" target="_blank">Report on the Inter&#8212;I.I.T. Sports Meet</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=100" target="_blank">N. C. C. Rifles</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=104" target="_blank">Our Library</a></p>
+                    <p><a href="<?=PUBLIC_URL?>pdf/annual-numbers/003.pdf#page=105" target="_blank">Pink Pages</a></p>                        
+                </div>
             </p>
         </div>
         <div class="post">
--- public/js/common.js
+++ public/js/common.js
@@ -36,6 +36,15 @@ $(document).ready(function() {
         $("html, body").animate({scrollTop: jumpLoc}, 1000);
     });
 
+    $("#TOCtoggle").click(function(){
+
+        $("#toc").slideToggle(1, function(){
+
+            buildMasonry();
+           
+        });
+    });  
+
     // $( '.email-submit' ).on('click', function(event){
 
     //     event.preventDefault();
@@ -65,45 +74,49 @@ jQuery(window).load(function () {
         columnWidth: '.post'
     });
     
-    
-    
     // This code fires every time a user resizes the screen and only affects .post elements
     // whose parent class isn't .container. Triggers resize first so nothing looks weird.
     
-    jQuery(window).bind('resize', function () {
-        if (!jQuery('#posts').parent().hasClass('container')) {
-            
-            
-            
-            // Resets all widths to 'auto' to sterilize calculations
-            
-            post_width = jQuery('.post').width() + gutter;
-            jQuery('#posts, body > #grid').css('width', 'auto');
-            
-            
-            
-            // Calculates how many .post elements will actually fit per row. Could this code be cleaner?
-            
-            posts_per_row = jQuery('#posts').innerWidth() / post_width;
-            floor_posts_width = (Math.floor(posts_per_row) * post_width) - gutter;
-            ceil_posts_width = (Math.ceil(posts_per_row) * post_width) - gutter;
-            posts_width = (ceil_posts_width > jQuery('#posts').innerWidth()) ? floor_posts_width : ceil_posts_width;
-            if (posts_width == jQuery('.post').width()) {
-                posts_width = '100%';
-            }
-            
-            
-            
-            // Ensures that all top-level elements have equal width and stay centered
-            
-            jQuery('#posts, #grid').css('width', posts_width);
-            // jQuery('#posts').css({'margin-left': '-20px'});
-                
-        
-        
-        }
-    }).trigger('resize');
-    
-
+    jQuery(window).bind('resize', buildMasonry()).trigger('resize');
 
 });
+
+function buildMasonry(){
+
+    var gutter = parseInt(jQuery('.post').css('marginBottom'));
+    var container = jQuery('#posts');
+
+
+    // Creates an instance of Masonry on #posts
+
+    container.masonry({
+        gutter: gutter,
+        itemSelector: '.post',
+        columnWidth: '.post'
+    });
+
+    if (!jQuery('#posts').parent().hasClass('container')) {
+    
+        // Resets all widths to 'auto' to sterilize calculations
+        
+        post_width = jQuery('.post').width() + gutter;
+        jQuery('#posts, body > #grid').css('width', 'auto');
+        
+        // Calculates how many .post elements will actually fit per row. Could this code be cleaner?
+        
+        posts_per_row = jQuery('#posts').innerWidth() / post_width;
+
+        floor_posts_width = (Math.floor(posts_per_row) * post_width) - gutter;
+        ceil_posts_width = (Math.ceil(posts_per_row) * post_width) - gutter;
+        posts_width = (ceil_posts_width > jQuery('#posts').innerWidth()) ? floor_posts_width : ceil_posts_width;
+        if (posts_width == jQuery('.post').width()) {
+            posts_width = '100%';
+        }
+        
+        // Ensures that all top-level elements have equal width and stay centered
+        
+        jQuery('#posts, #grid').css('width', '1325px');
+        // jQuery('#posts').css({'margin-left': '-20px'});
+    
+    }
+}
