983ab8b21e7fe67884652e9aadc26a69e0cbf1c8
shivasdst <shivably04sdst@gmail.com>
2016-07-23 14:47:28 +0530

    No. 121 - added async attribute for masonry and common js fiels

--- application/views/footer.php
+++ application/views/footer.php
@@ -13,9 +13,9 @@
 
     <!-- End Main page layout
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/masonry/3.1.5/masonry.pkgd.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/masonry/3.1.5/masonry.pkgd.min.js" async></script>
     
-    <script type="text/javascript" src="<?=PUBLIC_URL?>js/common.js"></script>
+    <script type="text/javascript" src="<?=PUBLIC_URL?>js/common.js" async></script>
    
 </body>
 </html>
