1db6994411468a4a8cd16f20b32b9710738adf6a
shivasdst <shivably04sdst@gmail.com>
2016-10-24 17:22:34 +0530

    No. 131 - updated config

--- application/config/config.php
+++ application/config/config.php
@@ -7,6 +7,7 @@ define('PHOTO_URL', PUBLIC_URL . 'Photos/');
 define('DOWNLOAD_URL', PUBLIC_URL . 'Downloads/');
 define('STOCK_IMAGE_URL', PUBLIC_URL . 'images/stock/');
 define('RESOURCES_URL', PUBLIC_URL . 'Resources/');
+define('JSON_PRECAST_URL', BASE_URL . 'json-precast/');
 
 // Physical location of resources
 define('PHY_BASE_URL', '/var/www/html/iitm/');
@@ -21,6 +22,13 @@ define('PHY_RESOURCES_URL', PHY_PUBLIC_URL . 'Resources/');
 
 define('DB_PREFIX', 'iitm');
 define('DB_HOST', 'localhost');
+
+// Git config
+define('GIT_USER_NAME', 'shivasdst232');
+define('GIT_PASSWORD', 'shiva565');
+define('GIT_REPO', 'github.com/SrirangaDigital/iitm.git');
+define('GIT_REMOTE', 'https://' . GIT_USER_NAME . ':' . GIT_PASSWORD . '@' . GIT_REPO);
+define('GIT_EMAIL', 'shiva@srirangadigital.com');
 
 // photo will become iitmPHOTO inside
 define('DB_NAME', 'heritage');
